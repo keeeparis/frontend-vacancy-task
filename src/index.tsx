8f4f5c7619a6dcc5f31070218e65aae53ea33915
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-06 12:53:42 +0600

    eslint add

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-// import React from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import AppRouter from './containers/AppRouter';
 
 createRoot(document.getElementById('root') as HTMLDivElement)
-    .render(<AppRouter />)
+  .render(<AppRouter />);
