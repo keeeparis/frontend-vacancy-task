3f8c1eb925b17a114aac24094377046b40257de4
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-10 17:04:47 +0600

    * add header, changed categoryHandler

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,32 +1,26 @@
+/* eslint-disable no-unused-vars */
 import React, {
-    useState, SyntheticEvent, useCallback, useEffect,
+    useState, useEffect, useCallback,
 } from 'react'
 
 import TokenItem from '../../components/TokenItem'
 import useTokens from '../../hooks/useTokens'
 import Button from '../../components/Button'
-import * as G from '../../styles'
+import Header from '../../containers/Header'
 import TabsWrap from './styles'
 
 import { tokens, categories } from '../../mock/tokens'
+import { Category } from '../../mock/types'
 
 const App = () => {
     const [category, setCategory] = useState(categories[0])
     const [loading, setLoading] = useState(false)
     const sortedTokens = useTokens(tokens, category.id)
 
-    const handleCategoryChange = useCallback((e: SyntheticEvent<HTMLButtonElement>) => {
-        const categoryCurrent = (e.target as HTMLButtonElement).dataset
-        if (category.id === categoryCurrent.id) return
-        if (categoryCurrent.id && categoryCurrent.title) {
-            const obj = {
-                id: categoryCurrent.id,
-                title: categoryCurrent.title,
-            }
-            setLoading(true)
-            setCategory(obj)
-        }
-    }, [category])
+    const handleCategoryChange = useCallback((item: Category) => () => {
+        setCategory(item)
+        setLoading(true)
+    }, [])
 
     useEffect(() => {
         setLoading(false)
@@ -34,22 +28,20 @@ const App = () => {
 
     return (
         <>
-            <G.Header>
-                <G.H1>Токены Everscale</G.H1>
+            <Header heading="Токены Everscale">
                 <TabsWrap>
                     {categories.map((item) => (
                         <Button
                             key={item.id}
-                            onClick={handleCategoryChange}
-                            data-id={item.id}
-                            data-title={item.title}
+                            onClick={handleCategoryChange(item)}
                             active={category.id === item.id}
                         >
                             {item.title}
                         </Button>
                     ))}
                 </TabsWrap>
-            </G.Header>
+            </Header>
+
             {!loading && sortedTokens.map((item) => (
                 <TokenItem key={item.id} item={item} />
             ))}
