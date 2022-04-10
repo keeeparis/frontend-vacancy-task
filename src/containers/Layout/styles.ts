7f231f682c804a6d8361cfe3310af6ae3be7e782
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-10 12:58:47 +0600

    padding layout fixed

--- src/containers/Layout/styles.ts
+++ src/containers/Layout/styles.ts
@@ -7,13 +7,12 @@ export const Wrapper = styled.div`
 export const Container = styled.div`
     width: 100%;
     height: 100vh;
-    padding: 0 112px 66px 128px;
+    padding: 0 128px 66px;
     border-radius: 32px 0 0 0;
     margin-left: -32px;
     z-index: 1;
     background-color: var(--page-bg-color);
     overflow-y: scroll;
-
 
     @media (max-width: 1600px) {
         padding: 0 64px 66px;
